f0fd990c92c2fa4c64af26c7e484d3e732301e38
syousafz <153879678+syousafz@users.noreply.github.com>
2024-07-04 13:49:29 -0400

    created functions to check for existing git url, and to add new pipeline entry to db

--- routes/create/create.py
+++ routes/create/create.py
@@ -3,20 +3,50 @@ from models.Pipeline import (
     PipelineOut,
     UpdatePipeline,
 )
+
 from typing import List, Dict
 from fastapi import Depends
-from motor.motor_asyncio import AsyncIOMotorDatabase
+from motor.motor_asyncio import AsyncIOMotorDatabase, AsyncIOMotorCollection
+from db import get_database
 
-#Recieve a github url, list of output files, path to the snakefile, path to pipeline configurations, path to conda environment
-def create_pipeline(data: Dict) -> Dict:
+database = get_database()
+snakemake_pipelines = database["snakemake_pipeline"]
+
+# Check if git_url exists in database
+async def git_url_exists(git_url: str, collection: AsyncIOMotorCollection) -> bool:
+    url = await collection.find_one({"git_url": git_url})
+    if url is not None:
+        return True
+    return False
+
+# CREATE
+# Add pipeline entry to database
+async def add_pipeline(
+    pipeline: CreatePipeline,
+    collection: AsyncIOMotorCollection,
+) -> PipelineOut:
+    new_pipeline = pipeline.model_dump()
+    result = await collection.insert_one(new_pipeline)
+    new_pipeline['id'] = result.inserted_id
+
+    return PipelineOut(**new_pipeline)
+
+# Recieve a pipeline name, github url, list of output files, path to the snakefile, path to pipeline configurations, path to conda environment
+async def create_pipeline(data: Dict) -> Dict:
     try:
+        pipeline_name: str = data["pipeline_name"]
         git_url: str = data["git_url"]
         output_files: List[str] = data["output_files"]
         snakefile_path: str = data["snakefile_path"]
         config_file_path: str = data["config_file_path"]
         conda_env_file_path: str = data["conda_env_file_path"]
 
-        return "test"
+        pipeline = CreatePipeline(**data)
+
+        if await git_url_exists(git_url, snakemake_pipelines):
+            raise ValueError("Git url already exists in database")
+        
+        return await add_pipeline(pipeline, snakemake_pipelines)
 
     except KeyError as e:
         raise ValueError(f"Missing required field: {e}")
